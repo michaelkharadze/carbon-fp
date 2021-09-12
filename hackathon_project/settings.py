8c5dd5b5d80971549a06cdd12493d0c4df1cd94d
unknown <mikhokh123@gmail.com>
2021-09-12 15:18:51 +0400

    Debug True

--- hackathon_project/settings.py
+++ hackathon_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '22efv*92xn2&)=g_o7ns*ntf1)r12op@3o7%b+nu0-=@15^j@4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hackathonclimate.herokuapp.com']
 
